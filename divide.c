9bdf00dc91b2d7e4e9ecd01ddbf033a35c3a18c1
Brandon Kriesten <brandonkriesten@Brandons-MacBook-Air.local>
2015-07-27 08:07:57 -0500

    Commented for readability

--- divide.c
+++ divide.c
@@ -1,3 +1,6 @@
+//This program is used in attenuation_measurement as a simple adding program to divide
+//2 numbers together. Called to do floating point division in bash.
+
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
