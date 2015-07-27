599dc46761bb306896412ccc54c1145290044319
Brandon Kriesten <brandonkriesten@Brandons-MacBook-Air.local>
2015-07-27 08:10:50 -0500

    Commented for readability

--- find_error.c
+++ find_error.c
@@ -1,3 +1,6 @@
+//This program takes two arguments, the rmsSum and number of entries, and finds the error
+//on the mean by taking the sqrt of the first and dividing by the second.
+
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
